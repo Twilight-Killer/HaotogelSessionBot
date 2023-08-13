f5f40b08cebfa8e4182bbe6b70ebe20bea31c9d2
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-08-13 15:36:38 +0700

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,7 +8,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\nɪɴɪ ᴀᴅᴀʟᴀʜ {Anony.mention},\nɪɴɪ ᴀᴅᴀʟᴀʜ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ʏᴀɴɢ ᴅɪ ʙᴜᴀᴛ ᴅᴀʀɪ ᴘʏᴛʜᴏɴ ᴅᴀɴ ᴘʏʀᴏɢʀᴀᴍ ꜱᴜᴍʙᴇʀ ᴛᴇʀʙᴜᴋᴀ",
+        text=f"ʜᴇʏ {message.from_user.first_name},\n\nꜱᴀʏᴀ {Anony.mention},\nꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ᴛᴇʟᴇᴛʜᴏɴ ᴅᴀɴ ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪ ᴛᴇʀʙᴀʀᴜ. ꜱᴀʏᴀ ᴀᴋᴀɴ ᴍᴇᴍʙᴀɴᴛᴜ ᴋᴀᴍᴜ ᴍᴇᴍʙᴜᴀᴛ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ᴅᴇɴɢᴀɴ ᴍᴜᴅᴀʜ ᴅᴀɴ ᴀᴍᴀɴ\nᴘᴏᴡᴇʀᴇᴅ ʙʏ @HaoTogelLivedraw",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
